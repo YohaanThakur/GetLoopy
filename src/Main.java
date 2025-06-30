2a89ddde7e414fb2163f333418ad2529b4a8b602
YohaanThakur <thakuryn@mail.uc.edu>
2025-06-30 17:28:53 -0400

    Completed Part A

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+class PartA {
+    public static void main(String[] args) {
+        // Task 1: Count up by 1 from 0 to 30
+        System.out.println("Task 1:");
+        for (int i = 0; i <= 30; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+
+        // Task 2: Count down by 1 from 30 to 0
+        System.out.println("Task 2:");
+        for (int i = 30; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+
+        // Task 3: Count up by 3 from 0 to 18
+        System.out.println("Task 3:");
+        for (int i = 0; i <= 18; i += 3) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+
+        // Task 4: Count down by 2 from 10 to 0
+        System.out.println("Task 4:");
+        for (int i = 10; i >= 0; i -= 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
